8d4192c77fe60454ae4159fa9de4e26ec9d2c16f
Tommaso Papini <tommy39@gmail.com>
2018-02-12 21:47:19 +0100

    Clock, Graph & Cubethon

---
+++ Cubethon/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float forwardForce = 400f;
+    public float sidewaysForce = 500f;
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d")) {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a")) {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if(rb.position.y < -1f) {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
