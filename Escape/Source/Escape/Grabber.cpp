d1cc69845624c8e97c18750006eed21164d25c52
Tommaso Papini <tommy39@gmail.com>
2018-09-16 17:13:52 +0100

    Escape S3L81

--- Escape/Source/Escape/Grabber.cpp
+++ Escape/Source/Escape/Grabber.cpp
@@ -1,6 +1,9 @@
 // Copyright Tommaso Papini 2018.
 
 #include "Grabber.h"
+#include "Engine/World.h"
+
+#define OUT
 
 
 // Sets default values for this component's properties
@@ -19,7 +22,7 @@ void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"))
 	
 }
 
@@ -29,6 +32,23 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Get the player view point this tick
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation
+	);
+
+	UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"),
+		*PlayerViewPointLocation.ToString(),
+		*PlayerViewPointRotation.ToString()
+	)
+
+	// Ray-cast out to reach distance
+
+	// See what we hit
+
+
 }
 
--- Escape/Source/Escape/PositionReport.cpp
+++ Escape/Source/Escape/PositionReport.cpp
@@ -20,7 +20,7 @@ void UPositionReport::BeginPlay()
 
 	FString ObjectName = GetOwner()->GetName();
 	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos);
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos)
 	
 }
 
