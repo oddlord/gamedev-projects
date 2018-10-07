733ebe5d86be3d1cbf231c3aa10f580404cd6b41
Tommaso Papini <tommy39@gmail.com>
2018-10-07 16:26:26 +0100

    Uniform variable translation S2L7

--- opengl/OpenGLCourseApp/OpenGLCourseApp/main.cpp
+++ opengl/OpenGLCourseApp/OpenGLCourseApp/main.cpp
@@ -1,12 +1,11 @@
 #include "stdafx.h"
 
+#include <cmath>
 #include <iostream>
 #include <string>
 
 #include <GLEW/glew.h>
 #include <GLFW/glfw3.h>
-
-using namespace std;
 
 // Window dimensions
 const GLint WIDTH = 800;
@@ -15,29 +14,37 @@ const GLint HEIGHT = 600;
 GLuint VAO;
 GLuint VBO;
 GLuint shaderProgram;
+GLuint uniformXMove;
+
+bool direction = true;
+float triOffset = 0.f;
+float triMaxOffset = 0.5f;
+float triIncrement = 0.0005f;
 
 enum InfoLogType { SHADER, PROGRAM };
 
 // Vertex shader
-static const char* vShader = "									\n\
-#version 330													\n\
-																\n\
-layout (location = 0) in vec3 pos;								\n\
-																\n\
-void main()														\n\
-{																\n\
-	gl_Position = vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);	\n\
+static const char* vShader = "											\n\
+#version 330															\n\
+																		\n\
+layout (location = 0) in vec3 pos;										\n\
+																		\n\
+uniform float xMove;													\n\
+																		\n\
+void main()																\n\
+{																		\n\
+	gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, pos.z, 1.0);	\n\
 }";
 
 // Fragment shader
-static const char* fShader = "									\n\
-#version 330													\n\
-																\n\
-out vec4 colour;												\n\
-																\n\
-void main()														\n\
-{																\n\
-	colour = vec4(1.0, 0.0, 0.0, 1.0);							\n\
+static const char* fShader = "											\n\
+#version 330															\n\
+																		\n\
+out vec4 colour;														\n\
+																		\n\
+void main()																\n\
+{																		\n\
+	colour = vec4(1.0, 0.0, 0.0, 1.0);									\n\
 }";
 
 void CreateTriangle()
@@ -50,20 +57,27 @@ void CreateTriangle()
 
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO); // bind VAO
-
+	{
 		glGenBuffers(1, &VBO);
 		glBindBuffer(GL_ARRAY_BUFFER, VBO); // bind VBO
+		{
 			glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 			// first attribute: layout location index in the vertex shader
 			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 			glEnableVertexAttribArray(0);
-
+		}
 		glBindBuffer(GL_ARRAY_BUFFER, 0); // unbind VBO
+	}
 	glBindVertexArray(0); // unbind VAO
 }
 
-int checkErrors(const GLuint target, const GLenum status, const InfoLogType infoLogType, string message)
+void PrintNL(std::string message)
+{
+	std::cout << message << std::endl;
+}
+
+int CheckErrors(const GLuint target, const GLenum status, const InfoLogType infoLogType, std::string message)
 {
 	GLint result = 0;
 	GLchar eLog[1024] = { 0 };
@@ -81,7 +95,7 @@ int checkErrors(const GLuint target, const GLenum status, const InfoLogType info
 			break;
 		}
 
-		cout << message << ": " << eLog << endl;
+		PrintNL(message + ": " + eLog);
 		return 1;
 	}
 
@@ -101,7 +115,7 @@ void AddShader(GLuint program, const char* shaderCode, GLenum shaderType)
 	glShaderSource(shader, 1, code, codeLength);
 	glCompileShader(shader);
 
-	if (!checkErrors(shader, GL_COMPILE_STATUS, InfoLogType::SHADER, "Error compiling the %d shader" + shaderType))
+	if (!CheckErrors(shader, GL_COMPILE_STATUS, InfoLogType::SHADER, "Error compiling the %d shader" + shaderType))
 	{
 		return;
 	}
@@ -115,7 +129,7 @@ void CompileShaders()
 	
 	if (!shaderProgram)
 	{
-		cout << "Error creating shader program!" << endl;
+		PrintNL("Error creating shader program!");
 		return;
 	}
 
@@ -127,17 +141,19 @@ void CompileShaders()
 
 	// Linking the shader program
 	glLinkProgram(shaderProgram);
-	if (!checkErrors(shaderProgram, GL_LINK_STATUS, InfoLogType::PROGRAM, "Error linking program"))
+	if (!CheckErrors(shaderProgram, GL_LINK_STATUS, InfoLogType::PROGRAM, "Error linking program"))
 	{
 		return;
 	}
 
 	// Validating the shader program
 	glValidateProgram(shaderProgram);
-	if (!checkErrors(shaderProgram, GL_VALIDATE_STATUS, InfoLogType::PROGRAM, "Error validating program"))
+	if (!CheckErrors(shaderProgram, GL_VALIDATE_STATUS, InfoLogType::PROGRAM, "Error validating program"))
 	{
 		return;
 	}
+
+	uniformXMove = glGetUniformLocation(shaderProgram, "xMove");
 }
 
 int main()
@@ -145,7 +161,7 @@ int main()
 	// Initialise GLFW
 	if (!glfwInit())
 	{
-		cout << "GLFW initialisation failed!" << endl;
+		PrintNL("GLFW initialisation failed!");
 		glfwTerminate();
 		return 1;
 	}
@@ -162,7 +178,7 @@ int main()
 	GLFWwindow* mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
 	if (!mainWindow)
 	{
-		cout << "GLFW window creation failed!" << endl;
+		PrintNL("GLFW window creation failed!");
 		glfwTerminate();
 		return 1;
 	}
@@ -181,7 +197,7 @@ int main()
 	// Initialise GLEW
 	if (glewInit() != GLEW_OK)
 	{
-		cout << "GLEW initialisation failed!" << endl;
+		PrintNL("GLEW initialisation failed!");
 		glfwDestroyWindow(mainWindow);
 		glfwTerminate();
 		return 1;
@@ -201,14 +217,33 @@ int main()
 		// Get and handle user input events
 		glfwPollEvents();
 
+		if (direction)
+		{
+			triOffset += triIncrement;
+		}
+		else
+		{
+			triOffset -= triIncrement;
+		}
+
+		if (abs(triOffset) >= triMaxOffset)
+		{
+			direction = !direction;
+		}
+
 		// Clear window
 		glClearColor(0.f, 0.f, 0.f, 1.f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shaderProgram);
+		{
+			glUniform1f(uniformXMove, triOffset);
 			glBindVertexArray(VAO);
+			{
 				glDrawArrays(GL_TRIANGLES, 0, 3);
+			}
 			glBindVertexArray(0);
+		}
 		glUseProgram(0);
 
 		glfwSwapBuffers(mainWindow);
