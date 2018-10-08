c8f4660c029cbafc8a0ece71194c5e3d984fa8aa
Tommaso Papini <tommy39@gmail.com>
2018-10-09 00:28:47 +0100

    3D pyramid S2L15

--- opengl/OpenGLCourseApp/OpenGLCourseApp/main.cpp
+++ opengl/OpenGLCourseApp/OpenGLCourseApp/main.cpp
@@ -20,10 +20,12 @@ const GLint HEIGHT = 600;
 
 const float toRadians = 3.14159265f / 180.f;
 
-GLuint VAO;
-GLuint VBO;
+GLuint VAO; // Vertex Array Object
+GLuint VBO; // Vertex Buffer Object
+GLuint IBO; // Index Buffer Object
 GLuint shaderProgram;
 GLuint uniformModel;
+GLuint uniformProjection;
 
 bool direction = true;
 float triOffset = 0.f;
@@ -31,7 +33,7 @@ const float triMaxOffset = 0.7f;
 const float triIncrement = 0.001f;
 
 float curAngle = 0.f;
-const float angleIncrement = 0.01f;
+const float angleIncrement = 0.1f;
 
 bool sizeDirection = true;
 float curSize = 0.4f;
@@ -47,47 +49,66 @@ static const char* vShader = "											\n\
 																		\n\
 layout (location = 0) in vec3 pos;										\n\
 																		\n\
+out vec4 vCol;															\n\
+																		\n\
 uniform mat4 model;														\n\
+uniform mat4 projection;												\n\
 																		\n\
 void main()																\n\
 {																		\n\
-	gl_Position = model * vec4(pos, 1.0);								\n\
+	gl_Position = projection * model * vec4(pos, 1.0f);					\n\
+	vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);							\n\
 }";
 
 // Fragment shader
 static const char* fShader = "											\n\
 #version 330															\n\
 																		\n\
+in vec4 vCol;															\n\
+																		\n\
 out vec4 colour;														\n\
 																		\n\
 void main()																\n\
 {																		\n\
-	colour = vec4(1.0, 0.0, 0.0, 1.0);									\n\
+	colour = vCol;														\n\
 }";
 
 void CreateTriangle()
 {
+	unsigned int indices[] = {
+		0, 3, 1,
+		1, 3, 2,
+		2, 3, 0,
+		0, 1, 2
+	};
+
 	GLfloat vertices[] = {
 		-1.f, -1.f, 0.f,
+		0.f, -1.f, 1.f,
 		1.f, -1.f, 0.f,
 		0.f, 1.f, 0.f
 	};
 
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO); // bind VAO
-	{
-		glGenBuffers(1, &VBO);
-		glBindBuffer(GL_ARRAY_BUFFER, VBO); // bind VBO
-		{
-			glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-			// first attribute: layout location index in the vertex shader
-			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-			glEnableVertexAttribArray(0);
-		}
-		glBindBuffer(GL_ARRAY_BUFFER, 0); // unbind VBO
-	}
+	glGenBuffers(1, &IBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO); // bind IBO
+
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); // bind VBO
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// first attribute: layout location index in the vertex shader
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0); // unbind VBO
 	glBindVertexArray(0); // unbind VAO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); // unbind IBO
 }
 
 void PrintNL(std::string message)
@@ -95,7 +116,7 @@ void PrintNL(std::string message)
 	std::cout << message << std::endl;
 }
 
-int CheckErrors(const GLuint target, const GLenum status, const InfoLogType infoLogType, std::string message)
+bool CheckErrors(const GLuint target, const GLenum status, const InfoLogType infoLogType, std::string message)
 {
 	GLint result = 0;
 	GLchar eLog[1024] = { 0 };
@@ -114,10 +135,10 @@ int CheckErrors(const GLuint target, const GLenum status, const InfoLogType info
 		}
 
 		PrintNL(message + ": " + eLog);
-		return 1;
+		return false;
 	}
 
-	return 0;
+	return true;
 }
 
 void AddShader(GLuint program, const char* shaderCode, GLenum shaderType)
@@ -133,10 +154,7 @@ void AddShader(GLuint program, const char* shaderCode, GLenum shaderType)
 	glShaderSource(shader, 1, code, codeLength);
 	glCompileShader(shader);
 
-	if (!CheckErrors(shader, GL_COMPILE_STATUS, InfoLogType::SHADER, "Error compiling the %d shader" + shaderType))
-	{
-		return;
-	}
+	CheckErrors(shader, GL_COMPILE_STATUS, InfoLogType::SHADER, "Error compiling the %d shader" + shaderType);
 
 	glAttachShader(program, shader);
 }
@@ -172,6 +190,7 @@ void CompileShaders()
 	}
 
 	uniformModel = glGetUniformLocation(shaderProgram, "model");
+	uniformProjection = glGetUniformLocation(shaderProgram, "projection");
 }
 
 int main()
@@ -221,6 +240,8 @@ int main()
 		return 1;
 	}
 
+	glEnable(GL_DEPTH_TEST);
+
 	// Setup viewport size
 	// Viewport = the part of the window where we can draw
 	// buffer width/height represent the ACTUAL width/height of the window
@@ -228,6 +249,8 @@ int main()
 
 	CreateTriangle();
 	CompileShaders();
+
+	glm::mat4 projection = glm::perspective(glm::radians(60.0f), (GLfloat)bufferWidth / (GLfloat)bufferHeight, 0.1f, 100.f);
 
 	// Loop until window close
 	while (!glfwWindowShouldClose(mainWindow))
@@ -271,26 +294,29 @@ int main()
 
 		// Clear window
 		glClearColor(0.f, 0.f, 0.f, 1.f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glUseProgram(shaderProgram);
-		{
-			glm::mat4 model(1.f);
-			model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.f, 0.f, 1.f));
-			model = glm::translate(model, glm::vec3(triOffset, 0.f, 0.f));
-			model = glm::scale(model, glm::vec3(curSize, curSize, 1.f));
 
-			//std::cout << glm::to_string(model) << std::endl;
-			//std::cin.ignore();
+		glm::mat4 model(1.f);
+		model = glm::translate(model, glm::vec3(triOffset, 0.f, -2.5f));
+		// model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.f, 1.f, 0.f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.f));
 
-			glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		// std::cout << glm::to_string(model) << std::endl;
+		// std::cin.ignore();
 
-			glBindVertexArray(VAO);
-			{
-				glDrawArrays(GL_TRIANGLES, 0, 3);
-			}
-			glBindVertexArray(0);
-		}
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+
+		glBindVertexArray(VAO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+			
+		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+
 		glUseProgram(0);
 
 		glfwSwapBuffers(mainWindow);
